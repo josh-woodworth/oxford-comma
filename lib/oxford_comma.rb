3ede729a2225a611e0c44b13aa335b23828594e7
Josh Woodworth <72992058+josh-woodworth@users.noreply.github.com>
2021-01-19 14:00:28 -0700

    commit tree dc6af7a4f8cf3f7d3755959359d415748b45d258

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
-
+    string = ""
+    if array.length == 2
+        array.join(" and ")
+    elsif array.length > 2
+        last_item = array.pop()
+        string = string + array.join(", ") + ", and " + last_item
+    else
+        array.join
+    end
 end
+
+# def oxford_comma(array)
+#     if array.size < 3 
+#         return array.join(' and ') 
+#     end
+#     puts array[-1]
+#     array[-1] = "and " + array[-1]
+#     puts array.join(", ")
+#     array.join(', ')
+#   end
